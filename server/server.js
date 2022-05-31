2a280e5e2f6bd07b792598cb8e9428e71d260ae4
ouch13 <insanecashmanguy@gmail.com>
2022-05-31 13:18:41 -0500

    bazinga

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const http = require('http');
+var express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '/public/');
+const port = process.env.PORT || 3000;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+app.use(express.static(publicPath));
+app.get('/public/', function(_req){
+  ;
+
+});
+server.listen(port, ()=> {
+    console.log(`Server is up on port ${port}.`)
+io.on('connection', (socket) => {
+        console.log('A user just connected.');
+        socket.on('disconnect', () => {
+            console.log('A user has disconnected.');
+        })
+    });
+});
